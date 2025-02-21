f6927c687aa5ff249389c743e36da1d00ef6f6cd
James Mathai <jammathai@gmail.com>
2025-02-21 15:13:41 -0500

    Update FlexInput.tsx

--- src/components/FlexInput.tsx
+++ src/components/FlexInput.tsx
@@ -6,14 +6,17 @@ export default function FlexInput({
   placeholder,
 }: Readonly<{ placeholder: string }>) {
   const inputRef = useRef<HTMLInputElement>(null);
-  let ctx: CanvasRenderingContext2D;
 
-  function measureText(text: string) {
-    return ctx.measureText(text).width + "px";
+  function resize() {
+    const input = inputRef.current!;
+    const ctx = document.createElement("canvas").getContext("2d")!;
+    ctx.font = getComputedStyle(input).font;
+    const textWidth = ctx.measureText(input.value || placeholder).width;
+    const padding = parseFloat(getComputedStyle(input).padding) * 2;
+    input.style.width = textWidth + padding + "px";
   }
 
   useEffect(() => {
-    ctx = document.createElement("canvas").getContext("2d")!;
     const event = new Event("input", { bubbles: true });
     inputRef.current!.dispatchEvent(event);
   }, []);
@@ -21,13 +24,9 @@ export default function FlexInput({
   return (
     <input
       ref={inputRef}
-      className="w-0 bg-transparent placeholder-placeholder outline-none"
+      className="w-0 p-1 rounded-md bg-transparent placeholder-placeholder"
       placeholder={placeholder}
-      onInput={() => {
-        const input = inputRef.current!;
-        ctx.font = getComputedStyle(input).font;
-        input.style.width = measureText(input.value || placeholder);
-      }}
+      onInput={resize}
     />
   );
 }
